4c0d30ea85bb22aab3941d426df3136953c3735f
Justyna Sadowska <korzyniewskaj@gmail.com>
2021-12-01 20:22:05 +0100

    15.4 correct

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,8 +6,8 @@ class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
-        {/* <h1 className={styles.title}>My first React List</h1> */}
-        {/* <h2 className={styles.subtitle}>Things to do</h2> */}
+        <h1 className={styles.title}>My first React App</h1>
+        <h2 className={styles.subtitle}>Hello World!</h2>
         <Hero />
       </section>
     )
