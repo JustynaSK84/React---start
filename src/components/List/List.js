9c49a8531422bd4e52e25f69a50383d74fc02acc
Justyna Sadowska <korzyniewskaj@gmail.com>
2021-12-05 19:53:39 +0100

    15.5 props

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
+import PropTypes from 'prop-types';
+import Column from '../Column/Column';
 
 class List extends React.Component {
+  static propTypes = {
+    title: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
+    image: PropTypes.string.isRequired,
+  }
+
+  static defaultProps = {
+    children: <p>I can do all the things!!!</p>,
+  }
+
   render() {
     return (
       <section className={styles.component}>
-        <h1 className={styles.title}>My first React App</h1>
-        <h2 className={styles.subtitle}>Hello World!</h2>
-        <Hero />
+        <Hero
+          titleText={this.props.title}
+          imageHero={this.props.image}
+        />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
+        <div className={styles.columns}>
+          <Column columnTitle={this.'Animals'} />
+          <Column columnTitle={this.'Plants'} />
+          <Column columnTitle={this.'Minerals'} />
+        </div>
       </section>
     )
   }
 }
 
-export default List
+export default List;
