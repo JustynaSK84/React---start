501e92ab50ca975486a10624dcdb46d16c9864b7
Justyna Sadowska <korzyniewskaj@gmail.com>
2022-01-10 23:07:58 +0100

    16.1 Eslint

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -10,17 +10,17 @@ import Creator from '../Creator/Creator';
 class List extends React.Component {
   state = {
     columns: this.props.columns || [],
-  }
+  };
   static propTypes = {
     title: PropTypes.node.isRequired,
     description: PropTypes.node,
     columns: PropTypes.array,
     image: PropTypes.string.isRequired,
-  }
+  };
 
   static defaultProps = {
     description: settings.defaultListDescription,
-  }
+  };
   addColumn(title){
     this.setState(state => (
       {
@@ -30,9 +30,9 @@ class List extends React.Component {
             key: state.columns.length ? state.columns[state.columns.length-1].key+1 : 0,
             title,
             icon: 'list-alt',
-            cards: []
-          }
-        ]
+            cards: [],
+          },
+        ],
       }
     ));
   }
@@ -48,16 +48,16 @@ class List extends React.Component {
           {ReactHtmlParser(this.props.description)}
         </div>
         <div className={styles.columns}>
-        {this.state.columns.map(({ key, ...columnProps }) => (
-          <Column key={key} {...columnProps} />
-        ))}
+          {this.state.columns.map(({ key, ...columnProps }) => (
+            <Column key={key} {...columnProps} />
+          ))}
         </div>
         <div className={styles.creator}>
           <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)} />
         </div>
 
       </section>
-    )
+    );
   }
 }
 
